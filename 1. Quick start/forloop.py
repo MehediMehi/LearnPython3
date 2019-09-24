dceba415e84c11cd98d1ca2145568021faeddf44
Mehedi Hasan Akash <mehaakmehi@gmail.com>
2019-09-24 15:06:07 +0600

    Update forloop.py

--- 1. Quick start/forloop.py
+++ 1. Quick start/forloop.py
@@ -5,8 +5,8 @@ Created on Mon Aug  5 18:53:13 2019
 
 @author: Mehedi Hasan Akash
 """
-#reaf the fles from the file
+#read the fles from the file
 
-fh = open('lines.txt', 'r')
+fh = open('lines.txt', 'r+')
 for line in fh.readlines():
     print(line, end = '')
