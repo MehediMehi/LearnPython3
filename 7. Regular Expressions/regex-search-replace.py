dae08217154fc5f91e33dc4b960d8ed356b886cf
Mehedi Hasan Akash <mehaakmehi@gmail.com>
2019-09-24 15:54:26 +0600

    Update regex-search-replace.py

--- 7. Regular Expressions/regex-search-replace.py
+++ 7. Regular Expressions/regex-search-replace.py
@@ -18,6 +18,5 @@ def main():
         match = re.search('(Len|Neverm)ore', line)
         if match:
             print(line.replace(match.group(), '###'), end='')
-        
-    
+
 if __name__ == "__main__": main()
