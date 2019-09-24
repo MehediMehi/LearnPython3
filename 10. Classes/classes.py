d49ebeb633fa01d36342965863dbdc5c5ac4439a
Mehedi Hasan Akash <mehaakmehi@gmail.com>
2019-09-24 15:15:25 +0600

    Update classes.py

--- 10. Classes/classes.py
+++ 10. Classes/classes.py
@@ -18,7 +18,7 @@ class Duck:
 	# object controller (set and get)
 	def set_value(self, value):
 		self._v = value
-
+    
 	def get_value(self):
 		return self._v
 
@@ -26,7 +26,7 @@ def main():
 	frank = Duck() # value encaptulated into the object 
 	frank.quack()
 	frank.walk()
-
+    
 	junk = Duck() # value encaptulated into the object 
 	junk._v = "Black Duck"
 	junk.quack()
@@ -35,7 +35,7 @@ def main():
 	donald = Duck("Gray Duck")
 	donald.quack()
 	donald.walk()
-
+    
 	print(donald.get_value()) # get the value
 	donald.set_value("Blue Duck") # set the value
 	print(donald.get_value())
