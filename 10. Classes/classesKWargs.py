1559865153ff15d0c4c82c0b94f5113c0275a77d
Mehedi Hasan Akash <mehaakmehi@gmail.com>
2019-09-24 15:29:08 +0600

    Update classesKWargs.py

--- 10. Classes/classesKWargs.py
+++ 10. Classes/classesKWargs.py
@@ -9,32 +9,30 @@ Created on Thu Sep 12 17:45:40 2019
 class Duck:
 	def __init__(self, **kwargs): # constructor, keyword arguments to store any number of inputs
 		self.variables = kwargs # _variableName makes the variable local, using dictionary
-
+	
 	# object controller (set and get)
 	def set_variables(self, k, v):
 		self.variables[k] = v
-
+	
 	def get_variables(self, k):
 		return self.variables.get(k, None)
 
 def main():    
-    #using dictionary
+	#using dictionary
 	donald = Duck(color = 'Gray')
 	print(donald.get_variables('color'))
 	print(donald.get_variables('feet'))
-
+	
 	kevin = Duck(feet = 2, color = 'black')
 	print(kevin.get_variables('color'))
 	print(kevin.get_variables('feet'))
-
+	
 	donald.set_variables('color', 'blue')
 	print(donald.get_variables('color'))
 	print(donald.get_variables('feet'))
-
+	
 	donald.set_variables('feet', 2)
 	print(donald.get_variables('color'))
 	print(donald.get_variables('feet'))
-
-	
 
 if __name__ == '__main__': main()
