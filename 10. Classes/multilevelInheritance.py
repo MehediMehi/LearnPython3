6b5de86c466f91db405d69fe898be624d4018a7e
Mehedi Hasan Akash <mehaakmehi@gmail.com>
2019-09-24 15:42:32 +0600

    Update multilevelInheritance.py

--- 10. Classes/multilevelInheritance.py
+++ 10. Classes/multilevelInheritance.py
@@ -10,30 +10,27 @@ Created on Thu Sep 15 12:48:44 2019
 class A:
     def __init__(self):
         print('Initializing: class A')
-
+    
     def sub_method(self, b):
         print('Printing from class A:', b)
-
 
 class B(A): # B is inheriting A class
     def __init__(self):
         print('Initializing: class B')
         super().__init__()
-
+    
     def sub_method(self, b):
         print('Printing from class B:', b)
         super().sub_method(b + 1)
-
 
 class C(B): # C is inheriting B class which itself inherites A class
     def __init__(self):
         print('Initializing: class C')
         super().__init__()
-        
+    
     def sub_method(self, b):
         print('Printing from class C:', b)
         super().sub_method(b + 1)
-
 
 if __name__ == '__main__':
     c = C()
